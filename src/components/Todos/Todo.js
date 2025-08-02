8a983edebcb3933cd46d4f1174bcdf0f37c6a5e4
Liudmyla Prorochenko <ljulju.prorochenko@gmail.com>
2025-08-02 15:12:18 +0300

    style: add styles to TodoForm, TodoList, and Todo components

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,12 +1,14 @@
-import { RiDeleteBin2Line, RiTodoFill } from "react-icons/ri";
-import { FaCheck } from "react-icons/fa";
+import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
+import styles from './Todo.module.css';
+
 function Todo({ todo, index }) {
   return (
-    <div >
-      <RiTodoFill />
-      <div>{todo.text}</div>
-      <RiDeleteBin2Line />
-      <FaCheck />
+    <div className={styles.todo}>
+      <RiTodoFill className={styles.todoIcon}/>
+      <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line className={styles.deleteIcon}/>
+      <FaCheck className={styles.checkIcon}/>
     </div>
   );
 }
--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,8 +1,9 @@
 import Todo from './Todo';
+import styles from './TodoList.module.css'
 
 function TodoList({ todos }) {
   return (
-    <div>
+    <div className={styles.todoListContainer}>
       {!todos.length && <h2>Todo list is empty</h2>}
       {todos.map((todo) => (
         <Todo key={todo.id} todo={todo} />
