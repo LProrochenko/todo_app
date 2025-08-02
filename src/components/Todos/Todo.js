787cb6290b61f3fa912910ceba5fb4b83381c019
Liudmyla Prorochenko <ljulju.prorochenko@gmail.com>
2025-08-02 14:54:28 +0300

    feat: add todo, delete, and check icons to Todo component

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,7 +1,12 @@
-function Todo({todo, index}) {
+import { RiDeleteBin2Line, RiTodoFill } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa";
+function Todo({ todo, index }) {
   return (
-    <div>
-      <p>{todo.text}</p>
+    <div >
+      <RiTodoFill />
+      <div>{todo.text}</div>
+      <RiDeleteBin2Line />
+      <FaCheck />
     </div>
   );
 }
