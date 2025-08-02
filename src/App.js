de35be3eede4d0286a859a26cfa8442b35e0d996
Liudmyla Prorochenko <ljulju.prorochenko@gmail.com>
2025-08-02 11:18:16 +0300

    feat: add ability to create new todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 import './App.css';
 import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  function addTodoHandler(text) {
+    const newTodo = {
+      text,
+      isCompleted: false,
+      id: uuidv4(),
+    };
+    setTodos([...todos, newTodo]);
+  }
+  console.log(todos);
+
   return (
     <div className="App">
       <h1>Todo App</h1>
-      <TodoForm />
-      <TodoList />
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} />
     </div>
   );
 }
--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,8 +1,17 @@
-function TodoForm() {
+import { useState } from 'react';
+
+function TodoForm({ addTodo }) {
+  const [text, setText] = useState('');
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+    addTodo(text);
+    setText('');
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Enter new todo" />
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" placeholder="Enter new todo" value={text} onChange={(e) => setText(e.target.value)}/>
         <button type="submit">Submit</button>
       </form>
     </div>
